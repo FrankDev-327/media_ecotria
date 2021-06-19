29d624da5ee0d0ff753fdd8c743b5910fcaa81db
knarf_ <francisco@getxplor.com>
2021-06-19 05:51:27 +0200

    making function to insert data N times

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose');
 const urlPar = { useNewUrlParser: true }
 const app = require('./app');
+const CronJobManager = require('cron-job-manager');
+const { PostModel } = require('./models/index')
 const conn = require('./setting/config');
 
 mongoose.Promise = global.Promise;
@@ -10,6 +12,8 @@ mongoose.set('debug', true)
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useUnifiedTopology', true);
+
+
 
 mongoose.connect(conn.db_name, urlPar, (err, res) => {
    if (err) {
@@ -24,3 +28,39 @@ mongoose.connect(conn.db_name, urlPar, (err, res) => {
       }
    }
 });
+
+async function insertDataIntoPostModel() {
+   try {
+      const listPrices = ['2004', '4514', '7522', '4742', '24521']
+      const listsNameProducts = ['caballo', 'ganado', 'cebolla', 'tomate', 'ajies', 'yuca', 'pepino']
+      const listCategory = ['industria', 'transporte', 'vegetables', 'ganaderia', 'ciraderos']
+
+
+      for await (let item of listPrices) {
+         const arrayData = {
+            "titlePost": "Test Listing" + Math.floor(Math.random() * (6 - 1) + 1),
+            "price": Math.floor(Math.random() * listPrices.length),
+            "descriptionPost": "Listado de prueba, lorem ipsum insofactum",
+            "catergory": Math.floor(Math.random() * listCategory.length),
+            "address": "Panamá",
+            "phoneNumber": "6080-6080",
+         }
+         let post = new PostModel({ ...arrayData })
+         await post.save();
+      }
+
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+
+const manager = new CronJobManager(
+   'callingInsertDataIntoPostModel',
+   '0-40 * 7-8 * * *',
+   () => {
+      insertDataIntoPostModel();
+   }
+)
+
+manager.start('callingInsertDataIntoPostModel');
